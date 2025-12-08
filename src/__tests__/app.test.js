1c454cde113231c0ef0c2b9a65b9bfedaafa11e0
mirAIlY-peek <miras.kural@mail.ru>
2025-12-08 14:20:01 +0500

    test commit

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -3,26 +3,23 @@ const { app, mongoose, Todo } = require('../index');
 
 describe('Todo API', () => {
 
-    // Перед тестами чистим базу (но пока у нас мок или in-memory, это просто для порядка)
-    // В реальном CI мы будем поднимать сервис MongoDB
+
     beforeAll(async () => {
-        // Ждем пока mongoose подключится (или упадет, если базы нет локально)
-        // Для локального запуска тестов без базы можно мокать,
-        // но мы будем поднимать базу в Docker Compose и CI, так что это ок.
+
     });
 
     afterAll(async () => {
-        await mongoose.connection.close(); // Закрываем соединение после тестов
+        await mongoose.connection.close();
     });
 
-    // Тест 1: Проверка Healthcheck
+
     it('GET /health should return 200 UP', async () => {
         const res = await request(app).get('/health');
         expect(res.statusCode).toEqual(200);
         expect(res.body.status).toEqual('UP');
     });
 
-    // Тест 2: Создание задачи
+
     it('POST /todos should create a new task', async () => {
         const task = { title: "Test Task via Jest" };
         const res = await request(app).post('/todos').send(task);
